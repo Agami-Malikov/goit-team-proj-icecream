bb68603bdc37f1ab5017c519b17fd1de0f25860b
Agami-Malikov <agami.malikov@gmail.com>
2022-04-25 23:39:45 +0300

    исправляет Customers

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+$('.cust-reviews__slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 1700,
+  infinite: false,
+});
+
+});
+
