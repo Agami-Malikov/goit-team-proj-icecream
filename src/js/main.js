7345794aef1855400e379b0a967da62564b8b215
Agami-Malikov <agami.malikov@gmail.com>
2022-04-26 11:22:47 +0300

    добавляет слайдер

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,10 @@
-$(function(){
 
 $('.cust-reviews__slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   dots: true,
-  autoplay: true,
-  autoplaySpeed: 1700,
   infinite: false,
 });
 
-});
 
