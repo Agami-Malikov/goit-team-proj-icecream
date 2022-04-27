543c79918b5ed4c392490e267d732b23b4bb3e1e
Jenia Berestian <jberestian@gmail.com>
2022-04-27 11:31:13 +0300

    created anchor

--- src/js/anchor.js
+++ src/js/anchor.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 2000);
     });
+
+    $("#menu1").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 2000);
+    });
 });
