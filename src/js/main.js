0cd9bccf465f3b243b446820053c6c1ca1630d04
Agami-Malikov <agami.malikov@gmail.com>
2022-04-28 04:02:24 +0300

    add prod btn

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,51 @@ $('.cust-reviews__slider').slick({
   infinite: false,
 });
 
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-desk-open]"),
+    closeModalBtn: document.querySelector("[data-desk-close]"),
+    modal: document.querySelector("[data-desk]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("open");
+  }
+})();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-desk-open2]"),
+    closeModalBtn: document.querySelector("[data-desk-close2]"),
+    modal: document.querySelector("[data-desk2]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("open2");
+  }
+})();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-desk-open3]"),
+    closeModalBtn: document.querySelector("[data-desk-close3]"),
+    modal: document.querySelector("[data-desk3]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("open3");
+  }
+})();
+
 
